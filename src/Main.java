f3775333e34da244257766a5531bf12998f89a45
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2023-11-22 16:50:43 +0100

    Update Main.java
    fixed som small issues

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,17 @@ public class Main {
         utill.printDeck(deck);
         int count = 0;
 
-
         // main loop
-        // checks if game is over.
         while (count < 1000){
             if (utill.isDone(deck)){
                 utill.printDeck(deck);
-                System.out.printf("%sbreaking!%s ", color.ANSI_GREEN, color.ANSI_RESET);
                 break;
             }
             count++;
             utill.crateNewStack(deck);
         }
 
-        System.out.printf("%stotal count: %d%s%n", color.ANSI_GREEN, count, color.ANSI_RESET);
+        System.out.printf("%s[DONE] total count: %d%s%n", color.ANSI_GREEN, count, color.ANSI_RESET);
 
     }
 
