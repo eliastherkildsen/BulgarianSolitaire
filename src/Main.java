793cd8ecbf59c4d346e5b5277313849a1e3bdd85
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2023-11-23 07:13:02 +0100

    made arraylist local.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
-    public static ArrayList<Integer> deck;
-
     public static void main(String[] args) {
         // setting up.
-        deck = Setup.setup();
+        ArrayList<Integer> deck = Setup.setup();
         utill.printDeck(deck);
         int count = 0;
 
         // main loop
         while (count < 1000){
+            // checks if game is over.
             if (utill.isDone(deck)){
+
+                // prints deck.
                 utill.printDeck(deck);
                 break;
             }
+            // increments count
             count++;
+            // creates new stack.
             utill.crateNewStack(deck);
         }
 
+        // status messages when game over.
         System.out.printf("%s[DONE] total count: %d%s%n", color.ANSI_GREEN, count, color.ANSI_RESET);
 
     }
